95687d6b1b1bdb9329542ecdaf332151e7134c2e
Matthew Herron <matthew.herron77@gmail.com>
2021-03-07 19:01:36 -0600

    Docker networking

--- bin/start.sh
+++ bin/start.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+echo Disconnecting from network...
+docker disconnect coinbase-swing-node swing-net
+docker disconnect coinbase-swing-nginx swing-net
+
+echo Removing network...
+docker network rm swing-net
+
+echo Creating network...
+docker network create --driver bridge swing-net
+
+echo Stopping containers...
 docker stop coinbase-swing-node
 docker stop coinbase-swing-nginx
 
+echo Removing containers...
 docker rm coinbase-swing-node
 docker rm coinbase-swing-nginx
 
+echo Removing images...
+docker rmi coinbase-swing-nginx:latest
+docker rmi coinbase-swing-node:latest
+
+echo Building images...
 docker build -f Dockerfile.nginx -t coinbase-swing-nginx:latest .
 docker build -f Dockerfile.node -t coinbase-swing-node:latest .
 
-docker run -d --name coinbase-swing-node -p 3333:3333 coinbase-swing-node:latest
-docker run -d --name coinbase-swing-nginx -p 80:80 coinbase-swing-nginx:latest
+echo Running containers
+docker run -d --name coinbase-swing-node --network swing-net -p 3333:3333 coinbase-swing-node:latest
+docker run -d --name coinbase-swing-nginx --network swing-net -p 80:80 coinbase-swing-nginx:latest
+
+# echo Connecting to network...
+# docker network connect coinbase-swing-node swing-net
+# docker network connect coinbase-swing-nginx swing-net
