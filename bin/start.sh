bef7f35b0e1be6b8edf5cf6d143df4cf380c062d
Matthew Herron <matthew.herron77@gmail.com>
2021-03-07 18:01:17 -0600

    Split docker containers

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-node server.js & nginx
+
+docker stop coinbase-swing-node
+docker stop coinbase-swing-nginx
+
+docker rm coinbase-swing-node
+docker rm coinbase-swing-nginx
+
+docker build -f Dockerfile.nginx -t coinbase-swing-nginx:latest .
+docker build -f Dockerfile.node -t coinbase-swing-node:latest .
+
+docker run -d --name coinbase-swing-node -p 3333:3333 coinbase-swing-node:latest
+docker run -d --name coinbase-swing-nginx -p 80:80 coinbase-swing-nginx:latest
